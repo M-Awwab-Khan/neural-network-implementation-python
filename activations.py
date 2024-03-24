d1050f1286467da1e8c8de0782b5df41d6e17770
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-03-24 13:10:16 +0500

    more activation functions

--- activations.py
+++ activations.py
@@ -6,3 +6,16 @@ def tanh(x):
 
 def derivative_tanh(x):
     return 1-np.tanh(x)**2
+
+def relu(x):
+    return np.maximum(0, x)
+
+def derivative_relu(x):
+    return (x > 0).astype(int)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def derivative_sigmoid(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+  

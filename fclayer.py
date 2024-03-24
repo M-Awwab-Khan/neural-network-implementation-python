0684cb5fe47bdd5821180ff329998d86d07ac64b
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-03-24 09:26:40 +0500

    fully connected layer till forward prop

---
+++ fclayer.py
@@ -0,0 +1,18 @@
+from layer import Layer
+import numpy as np
+
+# inherit from base class Layer 
+class FCLayer(Layer):
+
+    # input_size = number of input neurons
+    # output_size = number of output neurons
+    def __init__(self, input_size, output_size):
+        self.weights = np.random.rand(input_size, output_size) - 0.5
+        self.bias = np.random.rand(1, output_size) - 0.5
+
+    # returns output for a given input
+    def forward_propagate(self, input_data):
+        self.input = input_data
+        self.output = np.dot(input_data, self.weights) + self.bias
+
+        return self.output
